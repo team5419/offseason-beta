5ea9d94faa40023267006a1cd22e4410aec5cead
Ammelll <ammelsallow@gmail.com>
2025-09-16 19:09:18 -0700

    elevator classactual functions

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -4,6 +4,23 @@ package frc.robot.subsystems.elevator;
 public class Elevator {
 
 
+    public enum ElevatorGoal {
+      
+
+    }
+
+
+    public Elevator(ElevatorIO io){}
     
+
+    public void periodic(){}
+
+    public void resetPosition(){}
+
+    public void stop(){}
+
+    public void runVolts(){}
+
+    public void setDesiredLevel(ElevatorGoal goal){}
 }
 
