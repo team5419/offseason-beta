8a5ac5736ecdf24eb3ca9ef87f751522273455a9
PoultryPants <66968343+PoultryPants@users.noreply.github.com>
2025-10-07 18:58:27 -0700

    Worked on EndEffector subsystem file

--- src/main/java/frc/robot/subsystems/outtake/endeffector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/outtake/endeffector/EndEffector.java
@@ -11,7 +11,7 @@ import org.littletonrobotics.junction.Logger;
 
 public class EndEffector extends SubsystemBase {
 
-    private EndEffectorIO io;
+    private EndEffectorIO io;   
     private EndEffectorIOInputsAutoLogged inputs = new EndEffectorIOInputsAutoLogged();
 
     private static final LoggedTunableNumber kP = new LoggedTunableNumber("End Effector Roller/Gains/kP", kGains.kP());
@@ -49,6 +49,14 @@ public class EndEffector extends SubsystemBase {
                 kG,
                 kV,
                 kA);
+    }
+
+    public void runn(double volts) {
+        io.runVolts(volts);
+    }
+
+    public void stop() {
+        io.stop();
     }
 
     /**
