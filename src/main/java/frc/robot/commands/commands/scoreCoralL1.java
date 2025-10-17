c36ac2940905752c2892e5954d0cb0747ddcd37d
Paloma5499 <palomatorresslavik@students.berkeley.net>
2025-10-16 19:19:05 -0700

    worked on commands

--- src/main/java/frc/robot/commands/commands/scoreCoralL1.java
+++ src/main/java/frc/robot/commands/commands/scoreCoralL1.java
@@ -1,31 +1,32 @@
 package frc.robot.commands.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.intake.roller.IntakeRoller;
+import frc.robot.subsystems.intake.pivot.IntakePivot;
+import frc.robot.subsystems.intake.pivot.IntakePivot.IntakePivotGoal;
 import frc.robot.subsystems.beambreak.Beambreak;
 import frc.robot.subsystems.intake.roller.IntakeRoller.IntakeRollerGoal;
+import frc.robot.subsystems.intake.roller.IntakeRoller;
 
-public class scoreCoralL1 extends Command {
+public class scoreCoralL1 extends SequentialCommandGroup {
     private final IntakeRoller roller;
     private final Beambreak beamBreak;
+    private final IntakePivot intakePivot; 
 
     public scoreCoralL1(RobotContainer robot) {
         roller = robot.getIntakeRoller(); // fix later
         beamBreak = robot.getBeambreak();
+        intakePivot = robot.getIntakePivot();
+
+        addCommands(
+            //new InstantCommand(() -> intakePivot.setGoal(IntakePivotGoal.TO_SCOREL1()))
+            new WaitCommand(0.7),
+            new InstantCommand(() -> roller.setGoal(IntakeRollerGoal.OUTTAKECORRAL))
+
+        );
     }
 
-    public void initialize() {
-        
-    }
-
-    public void execute() {}
-
-    public boolean isFinished() {
-        return false;
-    }
-
-    public void end(boolean isFinished) {
-        roller.setGoal(IntakeRollerGoal.IDLE);
-    }
 }
--- src/main/java/frc/robot/subsystems/intake/roller/IntakeRoller.java
+++ src/main/java/frc/robot/subsystems/intake/roller/IntakeRoller.java
@@ -24,11 +24,14 @@ public class IntakeRoller extends SubsystemBase {
 
     private static final LoggedTunableNumber intake = new LoggedTunableNumber("Roller/Intake", -80.0);
     private static final LoggedTunableNumber outtakeEndEffector = new LoggedTunableNumber("Roller/OuttakeEndEffector", 80.0);
+    private static final LoggedTunableNumber outtakeCorral = new LoggedTunableNumber("Roller/OuttakeRoller", 80.0);
 
     public enum IntakeRollerGoal {
         IDLE(() -> 0), // Should be the current angle
         INTAKE(intake),
-        OUTTAKEENDEFFECTOR(outtakeEndEffector);
+        OUTTAKEENDEFFECTOR(outtakeEndEffector),
+        OUTTAKECORRAL(outtakeCorral);
+
 
 
         @Getter
