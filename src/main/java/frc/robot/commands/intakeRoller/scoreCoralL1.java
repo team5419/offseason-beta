d6070dfbc3cff578b64461d7a92f7b60c133f7ba
Ammelll <ammelsallow@gmail.com>
2025-10-28 19:16:50 -0700

    fixed errors in score coral L1

--- src/main/java/frc/robot/commands/intakeRoller/scoreCoralL1.java
+++ src/main/java/frc/robot/commands/intakeRoller/scoreCoralL1.java
@@ -3,28 +3,30 @@ package frc.robot.commands.intakeRoller;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.beambreak.Beambreak;
 import frc.robot.subsystems.intake.pivot.IntakePivot;
+import frc.robot.subsystems.intake.pivot.IntakePivot.IntakePivotGoal;
 import frc.robot.subsystems.intake.roller.IntakeRoller;
 import frc.robot.subsystems.intake.roller.IntakeRoller.IntakeRollerGoal;
 
-public class scoreCoralL1 extends SequentialCommandGroup {
+public class ScoreCoralL1 extends SequentialCommandGroup {
     private final IntakeRoller roller;
     private final Beambreak beamBreak;
     private final IntakePivot intakePivot;
 
-    public scoreCoralL1(RobotContainer robot) {
+    public ScoreCoralL1(RobotContainer robot) {
         roller = robot.getIntakeRoller(); // fix later
         beamBreak = robot.getBeamBreak();
         intakePivot = robot.getIntakePivot();
 
         addCommands(
-                new InstantCommand(() -> intakePivot.setGoal(IntakePivotGoal.TO_SCOREL1()),
+                new InstantCommand(() -> intakePivot.setGoal(IntakePivotGoal.TO_SCOREL1)),
                 new WaitCommand(0.7),
-                new InstantCommand(() -> roller.setGoal(IntakeRollerGoal.OUTTAKECORRAL())), 
+                new InstantCommand(() -> roller.setGoal(IntakeRollerGoal.OUTTAKECORRAL)),
                 new WaitUntilCommand(() -> !beamBreak.coralInIntake()),
-                new InstantCommand(intakeRoller.setGoal(IntakeRollerGoal.IDLE()))
+                new InstantCommand(() -> roller.setGoal(IntakeRollerGoal.IDLE)) // I dont know why theirs errors
                 );
     }
 }
