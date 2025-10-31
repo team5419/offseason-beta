96fdf24fa5e6054a1568557e83232fbceb3bf7c3
Ammelll <ammelsallow@gmail.com>
2025-10-30 17:54:18 -0700

    added timeout to handoff coral

--- src/main/java/frc/robot/commands/intakeRoller/HandoffCoral.java
+++ src/main/java/frc/robot/commands/intakeRoller/HandoffCoral.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.intakeRoller;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.beambreak.Beambreak;
 import frc.robot.subsystems.intake.pivot.IntakePivot;
@@ -18,6 +19,8 @@ public class HandoffCoral extends Command {
     private final Wrist wrist;
     private final IntakePivot pivot;
     private final Beambreak beamBreak;
+    private final Timer timer; 
+    private final Double timeOutTime = 10.0;
 
     public HandoffCoral(RobotContainer robot) {
         roller = robot.getIntakeRoller(); // fix later
@@ -25,22 +28,24 @@ public class HandoffCoral extends Command {
         beamBreak = robot.getBeamBreak();
         pivot = robot.getIntakePivot();
         wrist = robot.getWrist();
+        timer = new Timer();
     }
 
     public void initialize() {
         pivot.setGoal(IntakePivotGoal.TO_INTAKE_HANDOFF);
         wrist.setGoal(WristGoal.HANDOFF);
+        timer.start();
+    }
 
+    public void execute() {
         if (pivot.atGoal() && wrist.atGoal()) {
             roller.setGoal(IntakeRollerGoal.OUTTAKEENDEFFECTOR);
             endEffector.setGoal(EndEffectorRollerGoal.INTAKE);
         }
     }
 
-    public void execute() {}
-
     public boolean isFinished() {
-        return beamBreak.gamepieceInEndEffector();
+        return beamBreak.gamepieceInEndEffector() || timer.hasElapsed(timeOutTime); //in case of mechanical failure
     }
 
     public void end(boolean isFinished) {
