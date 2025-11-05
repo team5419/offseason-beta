cb1627da4f3ba4cee4a17fc1bc80a2dbea208fae
Paloma5499 <palomatorresslavik@students.berkeley.net>
2025-11-04 18:32:29 -0800

    add wrist subsystem to raisetoposition

--- src/main/java/frc/robot/commands/elevator/RaiseToPosition.java
+++ src/main/java/frc/robot/commands/elevator/RaiseToPosition.java
@@ -3,22 +3,29 @@ package frc.robot.commands.elevator;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorGoal;
+import frc.robot.subsystems.outtake.wrist.Wrist;
+import frc.robot.subsystems.outtake.wrist.Wrist.WristGoal;
 import java.util.function.Supplier;
 import frc.robot.RobotContainer;
 
 public class RaiseToPosition extends Command {
     private final Elevator elevator;
+    private final Wrist wrist;
     private final Supplier<ElevatorGoal> positionSupplier;
+    private final Supplier<WristGoal> wristSupplier;
 
-    public RaiseToPosition(RobotContainer robot, Supplier<ElevatorGoal> goal) {
+    public RaiseToPosition(RobotContainer robot, Supplier<ElevatorGoal> elevatorGoal, Supplier<WristGoal> wristGoal) {
         this.elevator = robot.getElevator();
-        positionSupplier = goal;
+        this.wrist = robot.getWrist();
+        positionSupplier = elevatorGoal;
+        wristSupplier = wristGoal;
         addRequirements(elevator, robot.getWrist());
     }
 
     @Override
     public void execute() {
         elevator.setCurrentGoal(positionSupplier.get());
+        // wrist.runPosition(wristSupplier.get());
     }
 
     @Override
