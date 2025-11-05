92887d90eec6a1be158c4c46ab4ab33b18c2f7b4
Anh <147779585+worker-bob@users.noreply.github.com>
2025-11-04 18:15:05 -0800

    Updated ports.java

--- src/main/java/frc/robot/constants/Ports.java
+++ src/main/java/frc/robot/constants/Ports.java
@@ -7,41 +7,143 @@ public class Ports {
     public static final int kOperatorPort = 1;
 
     // drivetrain
-    public static final int kFrontLeftDriveID = 0; // TODO: GET PORTS
-    public static final int kFrontLeftSteerID = 0; // TODO: GET PORTS
-    public static final int kFrontLeftEncoderID = 0; // TODO: GET PORTS
+    public static final int kFrontLeftDriveID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 10;
+                default -> 0;
+            };
+    public static final int kFrontLeftSteerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 20;
+                default -> 0;
+            };
+    public static final int kFrontLeftEncoderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 30;
+                default -> 0;
+            };
 
-    public static final int kFrontRightDriveID = 0; // TODO: GET PORTS
-    public static final int kFrontRightSteerID = 0; // TODO: GET PORTS
-    public static final int kFrontRightEncoderID = 0; // TODO: GET PORTS
+    public static final int kFrontRightDriveID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 11;
+                default -> 0;
+            };
+    public static final int kFrontRightSteerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 21;
+                default -> 0;
+            };
+    public static final int kFrontRightEncoderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 31;
+                default -> 0;
+            };
 
-    public static final int kBackLeftDriveID = 0; // TODO: GET PORTS
-    public static final int kBackLeftSteerID = 0; // TODO: GET PORTS
-    public static final int kBackLeftEncoderID = 0; // TODO: GET PORTS
+    public static final int kBackLeftDriveID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 13;
+                default -> 0;
+            };
+    public static final int kBackLeftSteerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 23;
+                default -> 0;
+            };
+    public static final int kBackLeftEncoderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 33;
+                default -> 0;
+            };
 
-    public static final int kBackRightDriveID = 0; // TODO: GET PORTS
-    public static final int kBackRightSteerID = 0; // TODO: GET PORTS
-    public static final int kBackRightEncoderID = 0; // TODO: GET PORTS
+    public static final int kBackRightDriveID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 12;
+                default -> 0;
+            };
+    public static final int kBackRightSteerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 22;
+                default -> 0;
+            };
+    public static final int kBackRightEncoderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 32;
+                default -> 0;
+            };
 
-    public static final int kPigeonID = 51;
+    public static final int kPigeonID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 51;
+                default -> 0;
+            };
 
-    public static final int kIntakeBeambreakPort = 0; // TODO: GET PORTS
-    public static final int kHandoffBeambreakPort = 0; // TODO: GET PORTS
-    public static final int kEndBeambreakPort = 0; // TODO: GET PORTS
+    public static final int kIntakeBeambreakPort =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 0; // TODO FIND VALUES
+                default -> 0;
+            };
+    public static final int kHandoffBeambreakPort =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 0; // TODO FIND VALUES
+                default -> 0;
+            };
+    public static final int kEndBeambreakPort =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 0; // TODO FIND VALUES
+                default -> 0;
+            };
 
-    public static final int kLedPort = 9; // TODO: GET PORTS
+    public static final int kLedPort =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 0; // TODO FIND VALUES
+                default -> 0;
+            };
 
-    // subsystem motors
-    public static final int kEleLeaderID = 19; // TODO: GET PORTS
-    public static final int kEleFollowerID = 18; // TODO: GET PORTS
+    public static final int kEleLeaderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 19;
+                default -> 0;
+            };
 
-    public static final int kIntakePivotID = 27; // TODO: GET PORTS
+    public static final int kEleFollowerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 18;
+                default -> 0;
+            };
 
-    public static final int kIntakeRollerLeaderID = 0; // TODO: GET PORTS
-    public static final int kIntakeRollerFollowerID = 0; // TODO: GET PORTS
+    public static final int kIntakePivotID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 69;
+                default -> 0;
+            };
 
-    public static final int kWristID = 0; // TODO: GET PORTS
+    public static final int kIntakeRollerLeaderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 66; // UPDATE when known
+                default -> 0;
+            };
 
-    public static final int kEndEffectorLeaderID = 0; // TODO: GET PORTS
-    public static final int kEndEffectorFollowerID = 0; // TODO: GET PORTS
+    public static final int kIntakeRollerFollowerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 65; // UPDATE when known
+                default -> 0;
+            };
+
+    public static final int kWristID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 79; // UPDATE when known
+                default -> 0;
+            };
+
+    public static final int kEndEffectorLeaderID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 76; // UPDATE when known
+                default -> 0;
+            };
+
+    public static final int kEndEffectorFollowerID =
+            switch (GlobalConstants.getRobotType()) {
+                case BETA -> 75; // UPDATE when known
+                default -> 0;
+            };
 }
