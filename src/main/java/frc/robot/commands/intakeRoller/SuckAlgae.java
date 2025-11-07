eea37d8e1ff5e7aa5590cb2145c83883653eba34
Paloma5499 <palomatorresslavik@students.berkeley.net>
2025-11-06 18:12:02 -0800

    formatting changes

--- src/main/java/frc/robot/commands/intakeRoller/SuckAlgae.java
+++ src/main/java/frc/robot/commands/intakeRoller/SuckAlgae.java
@@ -1,30 +1,30 @@
 package frc.robot.commands.intakeRoller;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.beambreak.Beambreak;
 import frc.robot.subsystems.outtake.endeffector.EndEffector;
 import frc.robot.subsystems.outtake.endeffector.EndEffector.EndEffectorRollerGoal;
-import frc.robot.subsystems.beambreak.Beambreak;
-import frc.robot.RobotContainer;
 
 public class SuckAlgae extends Command {
-    
+
     private final EndEffector endEffector;
     private final Beambreak beambreak;
 
-    public SuckAlgae(RobotContainer robot){
+    public SuckAlgae(RobotContainer robot) {
         endEffector = robot.getEndEffector();
         beambreak = robot.getBeamBreak();
     }
 
-    public void initialize(){
+    public void initialize() {
         endEffector.setGoal(EndEffectorRollerGoal.INTAKE);
     }
 
-    public boolean isFinished(){
+    public boolean isFinished() {
         return beambreak.gamepieceInEndEffector();
     }
 
-    public void end(){
+    public void end() {
         endEffector.setGoal(EndEffectorRollerGoal.HOLD);
     }
 }
