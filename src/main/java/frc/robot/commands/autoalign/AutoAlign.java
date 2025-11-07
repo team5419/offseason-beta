acc813ace83d2285633b6b8f628d320b6c06430e
Ammel <77129284+Ammelll@users.noreply.github.com>
2025-11-07 12:07:58 -0800

    add a placeholder for autoalign

---
+++ src/main/java/frc/robot/commands/autoalign/AutoAlign.java
@@ -0,0 +1,10 @@
+package frc.robot.commands.autoalign;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+
+public class AutoAlign extends Command {
+    public AutoAlign(RobotContainer robot) {
+        
+    }
+}
--- src/main/java/frc/robot/commands/score/ScoreCoral.java
+++ src/main/java/frc/robot/commands/score/ScoreCoral.java
@@ -5,7 +5,9 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
+import frc.robot.commands.autoalign.AutoAlign;
 import frc.robot.commands.elevator.RaiseToPosition;
+import frc.robot.commands.intakeRoller.OuttakeCoral;
 import frc.robot.subsystems.elevator.Elevator.ElevatorGoal;
 import frc.robot.subsystems.outtake.wrist.Wrist.WristGoal;
 
@@ -13,9 +15,9 @@ public class ScoreCoral extends SequentialCommandGroup {
     public ScoreCoral(RobotContainer robot, Supplier<ElevatorGoal> elevatorGoal, Supplier<WristGoal> wristGoal) {
         addRequirements(robot.getElevator(), robot.getWrist(), robot.getIntakePivot(), robot.getIntakeRoller());
         addCommands(new ParallelCommandGroup(
-                // TODO: auto align to reef
-                new RaiseToPosition(robot, elevatorGoal, wristGoal)
-                // TODO: outtake coral
+            new AutoAlign(robot),
+            new RaiseToPosition(robot, elevatorGoal, wristGoal),
+            new OuttakeCoral(robot)
         ));
     }
 }
